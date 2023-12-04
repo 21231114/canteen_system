bbfc00619931801fc5ee6fe3da92bab4961bc9a3
李孟晗 <21231114@buaa.edu.cn>
2023-12-04 17:28:13 +0800

    添加食物行为完成，目前bug:在进入到页面时，默认窗口不显示

--- app/src/main/java/com/example/myapplication/ShowFoodActivity.java
+++ app/src/main/java/com/example/myapplication/ShowFoodActivity.java
@@ -5,10 +5,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.myapplication.adapter.FoodListAdapter;
 import com.example.myapplication.db.FoodDbHelper;
 import com.example.myapplication.db.WindowDbHelper;
+import com.example.myapplication.dialog.AddFoodActivity;
 import com.example.myapplication.entity.FoodInfo;
 import com.example.myapplication.entity.WindowInfo;
 
@@ -25,7 +28,7 @@ public class ShowFoodActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //先插入一个食物，做做测试
-        FoodDbHelper.getInstance(ShowFoodActivity.this).addFood("测试食物","1","1");
+        //FoodDbHelper.getInstance(ShowFoodActivity.this).addFood("测试食物","1","1");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_food);
         //获取传输进来的要展示的食物信息
@@ -37,15 +40,31 @@ public class ShowFoodActivity extends AppCompatActivity {
         myFoodListAdapter = new FoodListAdapter();
         myRecycleView.setAdapter(myFoodListAdapter);//一定要记得为控件配备适配器
         loadData();//初始化数据
+        findViewById(R.id.add_food).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShowFoodActivity.this, AddFoodActivity.class);
+                intent.putExtra("canteen_name", my_canteen_name);
+                intent.putExtra("window_name", my_window_name);
+                startActivity(intent);
+            }
+        });
     }
 
-    //根据食堂名和窗口名来加载数据
     public void loadData() {
         foodList.clear();
         List<FoodInfo> foodInfoList = FoodDbHelper.getInstance(ShowFoodActivity.this).queryFoodListData(my_canteen_name, my_window_name);
         for (int i = 0; i < foodInfoList.size(); i++) {
             foodList.add(foodInfoList.get(i).getFood_name());
         }
+        //Toast.makeText(this, Integer.toString(foodList.size()), Toast.LENGTH_SHORT).show();
         myFoodListAdapter.setDataList(foodList);
+    }
+
+    @Override
+    protected void onResume() {
+        //从其他活动返回需要重新加载数据，因为可能数据库发生了变化
+        loadData();
+        super.onResume();
     }
 }
---
+++ app/src/main/java/com/example/myapplication/dialog/AddFoodActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.dialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+import com.example.myapplication.db.FoodDbHelper;
+
+public class AddFoodActivity extends AppCompatActivity {
+    private EditText et_food_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food);
+        //init
+        et_food_name = findViewById(R.id.et_food_name);
+        Intent intent = getIntent();
+        String canteen_name = intent.getStringExtra("canteen_name");
+        String window_name = intent.getStringExtra("window_name");
+        findViewById(R.id.commit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //食物名不是初始化时获取，是在提交时才知道!!!
+                String food_name = et_food_name.getText().toString();
+                FoodDbHelper myFoodDbHelper = FoodDbHelper.getInstance(AddFoodActivity.this);
+                //加入前需要判断该食物名是否在窗口里冲突
+                int row = myFoodDbHelper.addFood(food_name, canteen_name, window_name);
+                if(row>0){
+                    Toast.makeText(AddFoodActivity.this, "添加食物成功", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(AddFoodActivity.this, "添加食物失败", Toast.LENGTH_SHORT).show();
+                }
+                finish();
+            }
+        });
+    }
+}
